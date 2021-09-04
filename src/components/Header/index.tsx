51bfd155b2f4ff5c6fa6c86ae2fafcc4021b0dad
ChristianDaniel <christian_daniel04@hotmail.com>
2021-09-04 13:40:27 -0300

    header criado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Image from 'next/image'
+import LogoHeader from '../../../public/LogoPokedexTeste.png'
+import LogoHeader1 from '../../../public/pokedexLogo.png'
+
+// import { SeachPoke } from '../SeachPoke'
+
+// import { Headers } from './styles'
+
+const Header = (): JSX.Element => {
+  return (
+    <>
+      <Headers>
+        <Image src={LogoHeader} alt="logo" />
+
+        <div>
+          <Image src={LogoHeader1} alt="logo" />
+
+          <h3>
+            Bem-vindo ao Pokédex-App <br /> aqui você pode conhecer mais sobre seu pokemon favorito.
+          </h3>
+        </div>
+      </Headers>
+    </>
+  )
+}
+
+export default Header
