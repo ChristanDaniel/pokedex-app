b36ef675b6b0d6bee581de10ef45d160dabf07b6
ChristianDaniel <christian_daniel04@hotmail.com>
2021-09-05 05:03:37 -0300

    page do poke criada

---
+++ pages/pokemon/[...id].tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+const Header = dynamic(() => import('../../src/components/Header'), {
+  ssr: false
+})
+
+const SeachPoke = dynamic(() => import('../../src/components/SearchPoke'), {
+  ssr: false
+})
+
+const PokemonContainer = dynamic(() => import('../../src/features/Home/Pokemon/PokemonContainer'), {
+  ssr: false
+})
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Pokedex-app</title>
+      </Head>
+      <Header />
+      <SeachPoke />
+      <PokemonContainer />
+    </>
+  )
+}
+
+export default Home
