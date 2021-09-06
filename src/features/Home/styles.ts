1b3c7aabbeed08ac36800a249a246ad831f279ba
ChristianDaniel <christian_daniel04@hotmail.com>
2021-09-06 15:02:42 -0300

    css home

--- src/features/Home/styles.ts
+++ src/features/Home/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
-const MainContainer = styled.div``
+const MainContainer = styled.main`
+  width: auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 60px;
+  gap: 60px;
+`
 
 export { MainContainer }
