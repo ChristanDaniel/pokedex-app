aeab504d5e2f988d1d4e5737edea6e325f9c0dd8
ChristianDaniel <christian_daniel04@hotmail.com>
2021-09-06 15:13:33 -0300

    css HomeContainer

--- src/features/Home/styles.ts
+++ src/features/Home/styles.ts
@@ -6,6 +6,20 @@ const MainContainer = styled.main`
   grid-template-columns: repeat(3, 1fr);
   margin: 60px;
   gap: 60px;
+
+  a {
+    display: flex;
+    width: inherit;
+    /* background: ; */
+    height: 180px;
+    border-radius: 10px;
+    box-shadow: rgb(0 0 0 / 30%) 1px 3px 12px 0px;
+    transition: 0.2s;
+
+    :hover {
+      filter: brightness(0.9);
+    }
+  }
 `
 
 export { MainContainer }
