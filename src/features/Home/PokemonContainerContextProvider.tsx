27cad2042881cf26b246973a7690804634481265
ChristianDaniel <christian_daniel04@hotmail.com>
2021-09-19 12:47:37 -0300

    criando cotexto para PokeList

---
+++ src/features/Home/PokemonContainerContextProvider.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react'
+
+type PokemonType = {
+  type: {
+    name: string
+  }
+}
+
+type PokeProps = {
+  id: number
+  name: string
+  img: string
+  types?: PokemonType[]
+}
+
+interface IPokemonContainerProps {
+  pokeList: PokeProps[]
+  setPokeList: React.Dispatch<React.SetStateAction<PokeProps[]>>
+}
+
+const PokemonContainerContext = createContext<Partial<IPokemonContainerProps>>({})
+
+const PokemonContainerProvider: React.FC = ({ children }) => {
+  const [pokeList, setPokeList] = useState<PokeProps[]>([])
+
+  return <PokemonContainerContext.Provider value={{ pokeList, setPokeList }}>{children}</PokemonContainerContext.Provider>
+}
+
+export { PokemonContainerProvider, PokemonContainerContext }
