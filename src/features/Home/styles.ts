e6a01d1b61bb19fb7dd82b8f84d7dd8aac861f9f
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-03 14:23:08 -0300

    alterando cor do cardPokemon

--- src/features/Home/styles.ts
+++ src/features/Home/styles.ts
@@ -43,6 +43,48 @@ interface LiContentProps {
 const LiContent = styled.li<LiContentProps>`
   border-radius: 10px;
   /* /* background:  */
+  background: ${(props) => {
+    switch (props.pokeType) {
+      case 'fire':
+        return '#FFA756'
+      case 'water':
+        return '#58ABF6'
+      case 'bug':
+        return '#8BD674'
+      case 'dark':
+        return '#6F6E78'
+      case 'dragon':
+        return '#7383B9'
+      case 'electric':
+        return '#F2CB55'
+      case 'fairy':
+        return '#EBA8C3'
+      case 'fighting':
+        return '#83A2E3'
+      case 'flying':
+        return '#EB4971'
+      case 'ghost':
+        return '#8571BE'
+      case 'grass':
+        return '#8BBE8A'
+      case 'ground':
+        return '#F78551'
+      case 'ice':
+        return '#91D8DF'
+      case 'normal':
+        return '#B5B9C4'
+      case 'poison':
+        return '#9F6E97'
+      case 'psychic':
+        return '#FF6568'
+      case 'rock':
+        return '#D4C294'
+      case 'steel':
+        return '#4C91B2'
+      default:
+        return 'white'
+    }
+  }};
   /* box-shadow: rgb(0 0 0 / 30%); */
   box-shadow: rgb(0 0 0 / 30%) 1px 3px 12px 0px;
   max-height: 180px;
