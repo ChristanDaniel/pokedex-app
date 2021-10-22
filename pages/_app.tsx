26dd844125b0c57003c031d009e06d35492d09bd
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-22 14:04:32 -0300

    pequenas alterações

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import '../styles/Global'
 import type { AppProps } from 'next/app'
+
+import '../styles/Global'
 import { GlobalStyle } from '../styles/Global'
 
 function MyApp({ Component, pageProps }: AppProps) {
