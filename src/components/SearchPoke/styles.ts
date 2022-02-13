aa909f6bdeca4e43d93a6689393f112c82c6e959
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-13 15:44:55 -0300

    responsividade SeachPoke

--- src/components/SearchPoke/styles.ts
+++ src/components/SearchPoke/styles.ts
@@ -3,15 +3,13 @@ import styled from 'styled-components'
 const NavSection = styled.nav`
   background-color: #e2e2e2;
   padding-top: 10px;
-  /* width: auto; */
-  /* flex-direction: column; */
+
   padding-left: 50px;
   display: flex;
-  /* align-items: center; */
 
-  /* img {
-    display: flex;
-  } */
+  @media screen and (max-width: 1200px) {
+    display: block;
+  }
 
   input {
     margin-left: 12px;
