909e3661dc895b34928cd991cb03272f654aae62
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-18 22:47:39 -0300

    estrutura do footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export function Footer(): JSX.Element {
   return (
     <>
-      <footer>teste</footer>
+      <footer>
+        <div>
+          <div id="divContent">
+            <a href="https://github.com/ChristanDaniel" target="_blank" rel="noreferrer noopener">
+              <FaGithub /> Github
+            </a>
+            <span> | </span>
+            <a href="https://www.linkedin.com/in/christian-daniel-dev/" target="_blank" rel="noreferrer noopener">
+              <FaLinkedin /> Linkedin
+            </a>
+          </div>
+          <p>
+            <span>© {new Date().getFullYear()} Pokedex-app</span>
+            {' · '}
+            <span>Construido por Christian Daniel .</span>
+          </p>
+        </div>
+      </footer>
     </>
   )
 }
